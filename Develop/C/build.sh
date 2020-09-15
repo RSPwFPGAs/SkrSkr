c87aa8f2ff1d1b2d1ce3e0e87b00c898d6f7292e
pynq <pynq@xilinx.com>
2020-09-15 14:49:17 +0000

    Updated C simulation build script.

--- Develop/C/build.sh
+++ Develop/C/build.sh
@@ -1,3 +1,13 @@
+if [ -f "ap_int/master.zip" ]; then
+    echo "ap_int lib exists!"
+else
+    mkdir ap_int
+    cd ap_int/
+    wget https://github.com/Xilinx/HLS_arbitrary_Precision_Types/archive/master.zip
+    unzip master.zip
+    cd ..
+fi
+
 rm build -rf
 mkdir build
 cd build
